85ef4d78726c7ffb23d196921e50cad9dd5bb68f
TitleOS <titleos@titleos.dev>
2020-06-02 22:47:23 -0500

    Ported to .net 5, began on ConsoleInstance

---
+++ Console/ConsoleInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XboxResearchFramework.Win32;
+
+namespace XboxResearchFramework.Console
+{
+    public class ConsoleInfo
+    {
+        public static string SerialNumber;
+        public static string ConsoleID;
+
+        ConsoleInfo()
+        {
+            string Serial = "";
+            string ConsoleID = "";
+            Native.XblaGetConsoleSerialNumber(out Serial);
+            Native.XblaGetConsoleId(out ConsoleID);
+            SerialNumber = Serial;
+            ConsoleID = ConsoleID;
+        }
+    }
+}
